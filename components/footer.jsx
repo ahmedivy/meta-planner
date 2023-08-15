129134b835b2105e3352f0da22f8b37015f280c8
ahmed <ahmadabdullah4919@gmail.com>
2023-08-15 13:46:24 +0500

    add footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import { ModeToggle } from "./theme-toggle";
 
 function Footer() {
   return (
-    <footer className='flex items-center h-16 w-full'>
-      <p className="text-muted-foreground">
-        © 2023 Meta Planner Inc.
-      </p>
+    <footer className="flex items-center h-16 w-full">
+      <p className="text-muted-foreground">© 2023 Meta Planner Inc.</p>
+
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
