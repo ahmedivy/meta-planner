b799c0f155badad202b971ab6969b26fec363055
ahmed <ahmadabdullah4919@gmail.com>
2023-08-16 11:08:04 +0500

    add login link

--- components/auth-buttons.jsx
+++ components/auth-buttons.jsx
@@ -1,10 +1,13 @@
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 function AuthButtons({ className = "" }) {
   return (
     <div className={cn("hidden lg:flex gap-4 ml-auto", className)}>
-      <Button variant="outline">Log In</Button>
+      <Button variant="outline" asChild>
+        <Link href="/login">Log In</Link>
+      </Button>
       <Button>Sign Up</Button>
     </div>
   );
