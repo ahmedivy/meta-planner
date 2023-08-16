3dcf75141790b5519fb2b57dc2f7b4ccf004e94a
ahmed <ahmadabdullah4919@gmail.com>
2023-08-16 11:23:48 +0500

    fix padding for small screens

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/login-form"
 
 function Page() {
   return (
-    <div className="w-full flex flex-col items-center flex-grow pt-44">
+    <div className="w-full flex flex-col items-center flex-grow pt-20 lg:pt-40">
       <LoginForm />
     </div>
   )
