20ac442938834bd524b5cb91374ad08134ec861b
ahmed <ahmadabdullah4919@gmail.com>
2023-08-16 16:52:39 +0500

    trying to understand api

--- meta-api/account.py
+++ meta-api/account.py
@@ -38,8 +38,10 @@ async def meta_api_synchronization():
             print('MT5 account already added to MetaApi')
 
         #  wait until account is deployed and connected to broker
-        print('Deploying account')
-        await account.deploy()
+        if account.connection_status == "DISCONNECTED":
+            print('Deploying account')
+            await account.deploy()
+
         print('Waiting for API server to connect to broker (may take couple of minutes)')
         await account.wait_connected()
 
@@ -54,44 +56,6 @@ async def meta_api_synchronization():
         # invoke RPC API (replace ticket numbers with actual ticket numbers which exist in your MT account)
         print('Testing MetaAPI RPC API')
         print('account information:', await connection.get_account_information())
-        print('positions:', await connection.get_positions())
-        # print(await connection.get_position('1234567'))
-        print('open orders:', await connection.get_orders())
-        # print(await connection.get_order('1234567'))
-        print('history orders by ticket:', await connection.get_history_orders_by_ticket('1234567'))
-        print('history orders by position:', await connection.get_history_orders_by_position('1234567'))
-        print('history orders (~last 3 months):',
-              await connection.get_history_orders_by_time_range(datetime.utcnow() - timedelta(days=90),
-                                                                datetime.utcnow()))
-        print('history deals by ticket:', await connection.get_deals_by_ticket('1234567'))
-        print('history deals by position:', await connection.get_deals_by_position('1234567'))
-        print('history deals (~last 3 months):',
-              await connection.get_deals_by_time_range(datetime.utcnow() - timedelta(days=90), datetime.utcnow()))
-
-        print('server time', await connection.get_server_time())
-
-        # calculate margin required for trade
-        print('margin required for trade', await connection.calculate_margin({
-            'symbol': 'GBPUSD',
-            'type': 'ORDER_TYPE_BUY',
-            'volume': 0.1,
-            'openPrice': 1.1
-        }))
-
-        # trade
-        print('Submitting pending order')
-        try:
-            result = await connection.create_limit_buy_order('GBPUSD', 0.07, 1.0, 0.9, 2.0,
-                                                             {'comment': 'comm', 'clientId': 'TE_GBPUSD_7hyINWqAlE'})
-            print('Trade successful, result code is ' + result['stringCode'])
-        except Exception as err:
-            print('Trade failed with error:')
-            print(api.format_error(err))
-
-        # finally, undeploy account after the test
-        print('Undeploying MT5 account so that it does not consume any unwanted resources')
-        await connection.close()
-        await account.undeploy()
 
     except Exception as err:
         # process errors
