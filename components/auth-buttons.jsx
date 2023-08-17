96d1a22da5d24bd237840e5d670b2d0c225b7444
ahmed <ahmadabdullah4919@gmail.com>
2023-08-17 14:45:10 +0500

    fix bug

--- components/auth-buttons.jsx
+++ components/auth-buttons.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 function AuthButtons({ className = "" }) {
   return (
     <div className={cn("hidden lg:flex gap-4 ml-auto", className)}>
-      <Button variant="outline" asChild tabindex="-1">
+      <Button variant="outline" asChild tabIndex="-1">
         <Link href="/login">Log In</Link>
       </Button>
-      <Button tabindex="-1">Sign Up</Button>
+      <Button tabIndex="-1">Sign Up</Button>
     </div>
   );
 }
