2c40898ee3830aa30d66ad355bd8db9f8e0ed472
ahmed <ahmadabdullah4919@gmail.com>
2023-08-20 16:25:57 +0500

    add page

--- app/(portal)/quotes/page.jsx
+++ app/(portal)/quotes/page.jsx
@@ -1,24 +1,21 @@
-import { getServerSession } from "next-auth";
+import Link from "next/link";
 
-import { authOptions } from "@/lib/auth";
-import { getSymbols } from "@/lib/meta-api/account";
-
-async function getData() {
-  const session = await getServerSession(authOptions);
-  const { user } = session;
-  const symbols = await getSymbols(user.accountId);
-
-  return {
-    symbols,
-  };
-}
+import { Button } from "@/components/ui/button";
 
 async function Page() {
-  const { symbols } = await getData();
-
   return (
-    <main>
-      <pre>{JSON.stringify(symbols, null, 2)}</pre>
+    <main className="w-full lg:pl-6 pt-4 flex flex-col gap-4">
+      <div className="flex flex-col md:flex-row md:items-center justify-between w-full gap-4">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-3xl font-black">Quotes</h1>
+          <p className="text-muted-foreground text-xl">
+            Symbols you are tracking and their real time stats
+          </p>
+        </div>
+        <Button asChild className="font-bold" size="lg">
+          <Link href="/quotes/configure">Configure Quotes</Link>
+        </Button>
+      </div>
     </main>
   );
 }
